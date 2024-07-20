28dba387b5a58b2d9400477f0f80b41078f3792d
taajik <75927372+taajik@users.noreply.github.com>
2024-07-20 18:46:30 +0330

    calculate pixels of data inside carrier file

--- steganographer.py
+++ steganographer.py
@@ -1,14 +1,17 @@
 
 import base64
+import math
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
+from PIL import Image
 
 
 password = input("pw: ").encode()
-file = "/home/aaronn/Documents/TEMP/Camel_Fingertips2.mp3"
+file = "../Camel_Fingertips2.mp3"
+carrier_file = "LDR_3_VPM_VISTA_STILL_digital_art_FINAL.png"
 
 
 def gen_key(pw):
@@ -24,35 +27,38 @@ def gen_key(pw):
     return base64.urlsafe_b64encode(kdf.derive(pw)).decode()
 
 
-def stego_encrypt(file, pw=None):
+def stego_encrypt(carrier_file, payload_file, pw=None):
     """"""
 
-    with open(file, "rb") as bf:
-        data = bf.read()
+    img = Image.open(carrier_file)
+    width, height = img.size
+    pix = img.load()
+
+    with open(payload_file, "rb") as bf:
+        payload = bf.read()
 
     if pw is not None:
         key = gen_key(pw)
         cipher = Fernet(key)
-        encrypted_data = cipher.encrypt(data)
+        payload = cipher.encrypt(payload)
 
-    print(type(data))
-    print(data[:100])
-    print(list(data[:100]))
-    print(type(encrypted_data))
-    print(encrypted_data[:100])
+    # Calculate density of payload within carrier's pixels.
+    carrier_capacity = width*height - 4
+    data_size = math.ceil(len(payload) * 8 / 6)
+    pixel_steps = math.floor(carrier_capacity / data_size)
+    print(carrier_capacity, data_size, pixel_steps)
 
 
-# stego_encrypt(file)
-stego_encrypt(file, password)
-
+stego_encrypt(carrier_file, file)
 
 
 
 
-def stego_decrypt(file, pw=None):
+
+def stego_decrypt(stego_file, pw=None):
     """"""
 
-    with open(file, "rb") as bf:
+    with open(stego_file, "rb") as bf:
         data = bf.read()
 
     # Do steganography here!
