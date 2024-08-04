26c02c1ded4893d86267035c24a6a4fddd2dee2c
taajik <75927372+taajik@users.noreply.github.com>
2024-08-04 15:12:46 +0330

    bold the selected item in menus

--- stego/static/stego/js/payload-menu.js
+++ stego/static/stego/js/payload-menu.js
@@ -1,11 +1,15 @@
 
+var payload_text_label = document.getElementById('payload_text_label');
+var payload_file_label = document.getElementById('payload_file_label');
 var payload_text_field = document.getElementById('payload_text_field');
 var payload_file_field = document.getElementById('payload_file_field');
 var payload_text_input = document.getElementById('id_payload_text');
 var payload_file_input = document.getElementById('id_payload_file');
 var payload_label = document.getElementById('payload_label');
 
-document.getElementById('payload_text_label').addEventListener('click', function() {
+payload_text_label.addEventListener('click', function() {
+    payload_text_label.style.fontWeight = 'bold';
+    payload_file_label.style.fontWeight = 'normal';
     payload_text_field.style.display = 'block';
     payload_text_input.required = true;
     payload_file_field.style.display = 'none';
@@ -14,7 +18,9 @@ document.getElementById('payload_text_label').addEventListener('click', function
     payload_label.htmlFor = 'id_payload_text';
 });
 
-document.getElementById('payload_file_label').addEventListener('click', function() {
+payload_file_label.addEventListener('click', function() {
+    payload_file_label.style.fontWeight = 'bold';
+    payload_text_label.style.fontWeight = 'normal';
     payload_file_field.style.display = 'block';
     payload_file_input.required = true;
     payload_text_field.style.display = 'none';
