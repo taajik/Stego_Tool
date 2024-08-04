96b4acc2704a3c6f2ed5999506c9ec327375e1d8
taajik <75927372+taajik@users.noreply.github.com>
2024-08-04 12:22:35 +0330

    add show/hide button to password field

--- stego/static/stego/js/password-field.js
+++ stego/static/stego/js/password-field.js
@@ -1,6 +1,7 @@
 
 var arrow_icon = document.getElementById('arrow_icon');
 var password_field = document.getElementById('password_field');
+var password_input = document.getElementById('id_password');
 
 document.getElementById('cipher_option').addEventListener('click', function() {
     arrow_icon.classList.toggle('arrow_up');
@@ -8,8 +9,25 @@ document.getElementById('cipher_option').addEventListener('click', function() {
 
     if (password_field.style.display === 'block') {
         password_field.style.display = 'none';
-        document.getElementById('id_password').value = '';
+        password_input.value = '';
     } else {
         password_field.style.display = 'block';
     }
 });
+
+
+var showhide_btn = document.getElementById('showhide_btn');
+
+function showHidePW() {
+    if (password_input.type === 'password') {
+        password_input.type = 'text';
+    } else {
+        password_input.type = 'password';
+    }
+
+    if (showhide_btn.innerText === 'show') {
+        showhide_btn.innerText = 'hide';
+    } else {
+        showhide_btn.innerText = 'show';
+    }
+}
